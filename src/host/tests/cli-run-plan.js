2ffeb44c22d0369f011b307eec0e8e70ac981d6c
Michael "Z" Goddard <mzgoddard@gmail.com>
2022-01-14 23:26:15 -0500

    fixup! fix: test src/shared and src/host files

--- src/host/tests/cli-run-plan.js
+++ src/host/tests/cli-run-plan.js
@@ -5,29 +5,13 @@ import { fileURLToPath } from 'url';
 import test from 'ava';
 import { HostMessage } from '../messages.js';
 
-const VERBOSE = [
-  '--verbose',
-  [
-    HostMessage.START,
-    HostMessage.UNCAUGHT_ERROR,
-    HostMessage.WILL_STOP,
-    HostMessage.SERVER_LISTENING,
-    HostMessage.ADD_SERVER_DIRECTORY,
-    HostMessage.REMOVE_SERVER_DIRECTORY,
-    HostMessage.PLAN_READ,
-  ].join(','),
-];
-
 test('plan1', async t => {
   t.snapshot(
     await spawnRunPlan([
       '--plan-workingdir=fixtures/host-bin/plan1',
       '"**"',
       '--agent-mock',
-      '--agent-mock-open-page=skip',
-      ...VERBOSE,
-      // '--verbose',
-      // '"start,willStop,startAgent,startServer,stopServer,planRead,stopAgent,startTest"',
+      '--debug',
     ])
   );
 });
@@ -38,9 +22,7 @@ test('plan2', async t => {
       '--plan-workingdir=fixtures/host-bin/plan2',
       '"**"',
       '--agent-mock',
-      '--agent-mock-open-page',
-      'skip',
-      ...VERBOSE,
+      '--debug',
     ])
   );
 });
@@ -51,17 +33,11 @@ test('plan3', async t => {
       '--plan-workingdir=fixtures/host-bin/plan3',
       '"**"',
       '--agent-mock',
-      '--agent-mock-open-page',
-      'skip',
-      ...VERBOSE,
+      '--debug',
     ])
   );
 });
 
-const ISO_DATE = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/g;
-const DETERMINISTIC_DATE = '2000-01-01T12:00:00.000Z';
-const URL_PORT_PATHNAME = /(https?:\/\/[^:]+:)(\d+)(\/\w+)?/g;
-const DETERMINISTIC_PORT_PATHNAME = (...match) => `${match[1]}8888${match[3] ? '/static' : ''}`;
 async function spawnRunPlan(args) {
   const dirname = path.dirname(fileURLToPath(import.meta.url));
   const hostBin = path.join(dirname, '../../../bin/host.js');
@@ -81,13 +57,21 @@ async function spawnRunPlan(args) {
   );
 
   return {
-    stdout: stdout
-      .toString()
-      .replace(ISO_DATE, DETERMINISTIC_DATE)
-      .replace(URL_PORT_PATHNAME, DETERMINISTIC_PORT_PATHNAME),
-    stderr: stderr
-      .toString()
-      .replace(ISO_DATE, DETERMINISTIC_DATE)
-      .replace(URL_PORT_PATHNAME, DETERMINISTIC_PORT_PATHNAME),
+    stdout: replaceNonDeterministicOutput(stdout.toString()),
+    stderr: replaceNonDeterministicOutput(stderr.toString()),
   };
+}
+
+const ISO_DATE = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/g;
+const DETERMINISTIC_DATE = '2000-01-01T12:00:00.000Z';
+const URL_PATHNAME_ONLY = /'\/[^/']+((?:\/[^/']*)*')/g;
+const DETERMINISTIC_PATHNAME_PREFIX = (...match) => `'/static${match[1]}`;
+const URL_PORT_PATHNAME = /(https?:\/\/[^:]+:)\d+(\/[^/]+)?/g;
+const DETERMINISTIC_PORT_PATHNAME = (...match) => `${match[1]}8888${match[2] ? '/static' : ''}`;
+
+function replaceNonDeterministicOutput(text) {
+  return text
+    .replace(ISO_DATE, DETERMINISTIC_DATE)
+    .replace(URL_PATHNAME_ONLY, DETERMINISTIC_PATHNAME_PREFIX)
+    .replace(URL_PORT_PATHNAME, DETERMINISTIC_PORT_PATHNAME);
 }
