cf5a0caa5203179ddb294398653a2f789786427e
Mx. Corey Frang <gnarf37@gmail.com>
2023-11-09 15:41:23 -0500

    add ONE-ZERO & COMMA

--- src/agent/at-driver.js
+++ src/agent/at-driver.js
@@ -177,6 +177,18 @@ export const webDriverCodePoints = {
   META: '\ue03d',
   COMMAND: '\ue03d',
   ZENKAKU_HANKAKU: '\ue040',
+
+  ONE: '1',
+  TWO: '2',
+  THREE: '3',
+  FOUR: '4',
+  FIVE: '5',
+  SIX: '6',
+  SEVEN: '7',
+  EIGHT: '8',
+  NINE: '9',
+  ZERO: '0',
+  COMMA: ',',
 };
 
 export class ATKey {
