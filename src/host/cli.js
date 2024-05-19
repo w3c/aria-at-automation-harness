9f8f10ed69d71b3097920b943a1c9ccdc86ca6f5
Mike Pennisi <mike@mikepennisi.com>
2024-05-19 13:40:57 -0400

    Correct types: src/host/cli.js

--- src/host/cli.js
+++ src/host/cli.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /// <reference path="./types.js" />
 
 /**
@@ -11,6 +10,14 @@ import { hideBin } from 'yargs/helpers';
 import * as readPlanCommand from './cli-read-plan.js';
 import * as runPlanCommand from './cli-run-plan.js';
 
+/**
+ * @param {object} options
+ * @param {import("events").EventEmitter} options.signals
+ * @param {function(*): void} [options.send]
+ * @param {import("events").EventEmitter} options.stdin
+ * @param {import("events").EventEmitter} options.stdout
+ * @param {import("events").EventEmitter} options.stderr
+ */
 export async function createParser({ signals, send, stdin, stdout, stderr }) {
   return /** @type {yargs} */ (await yargs())
     .middleware(argv => {
@@ -24,6 +31,15 @@ export async function createParser({ signals, send, stdin, stdout, stderr }) {
     .command(readPlanCommand);
 }
 
-export async function parse({ argv = [], ...parserConfiguration } = {}) {
+/**
+ * @param {object} options
+ * @param {string[]} options.argv
+ * @param {import("events").EventEmitter} options.signals
+ * @param {function(*): void} [options.send]
+ * @param {import("events").EventEmitter} options.stdin
+ * @param {import("events").EventEmitter} options.stdout
+ * @param {import("events").EventEmitter} options.stderr
+ */
+export async function parse({ argv = [], ...parserConfiguration }) {
   return (await createParser(parserConfiguration)).parse(hideBin(argv));
 }
