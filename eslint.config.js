fa4902af34ce219970c7866220318c4e6bc8e9f4
Chris Cuellar <58723+ChrisC@users.noreply.github.com>
2024-08-19 15:34:48 -0700

    removes eslint and typescript plugins
    with default rule configs. We can add these back later if needed

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,6 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,mjs,cjs}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
