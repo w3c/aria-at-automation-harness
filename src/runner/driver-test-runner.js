3e28f2c2ccb7e7e68c65834f421ec83ed8cd9580
Mx Corey Frang <corey@bocoup.com>
2025-07-14 19:07:14 -0400

    Temporary fix for jaws double escape (#89)
    * double parse speech strings on JAWS
    
    * add comment to issue

--- src/runner/driver-test-runner.js
+++ src/runner/driver-test-runner.js
@@ -347,8 +347,15 @@ export class DriverTestRunner {
    */
   async _collectSpeech(debounceDelay, asyncOperation) {
     let spoken = [];
+    const isJAWS = (await this.collectedCapabilities).atName == 'JAWS';
+
     const speechJob = startJob(async signal => {
-      for await (const speech of signal.cancelable(this.atDriver.speeches())) {
+      for await (let speech of signal.cancelable(this.atDriver.speeches())) {
+        if (isJAWS) {
+          // temporary workaround to double-escaped string literals.
+          // see https://github.com/w3c/aria-at-automation-harness/issues/90
+          speech = JSON.parse(`"${speech}"`);
+        }
         spoken.push(speech);
         this.log(RunnerMessage.SPEECH_EVENT, { spokenText: speech });
       }
