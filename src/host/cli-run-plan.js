32b1960c3a44f9ddd6bad4d41b90b4f6f1079f42
Corey Frang <gnarf37@gmail.com>
2025-09-17 14:26:40 -0400

    Add Edge support

--- src/host/cli-run-plan.js
+++ src/host/cli-run-plan.js
@@ -77,7 +77,7 @@ export const builder = (args = yargs) =>
         default: 'http://localhost:4444',
       },
       'web-driver-browser': {
-        choices: ['chrome', 'firefox', 'safari'],
+        choices: ['chrome', 'firefox', 'safari', 'edge'],
         default: 'firefox',
       },
       'at-driver-url': {
